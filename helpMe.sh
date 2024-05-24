ee61caef01de2cffc07cced027e5b6c21cf7bea9
chad <chadb78999@gmail.com>
2024-05-23 21:51:51 -0500

    added to help script

--- helpMe.sh
+++ helpMe.sh
@@ -5,3 +5,5 @@ echo "* 1. Run python3 constructBashArray.py *"
 echo "* 2. Enter commands you want to add to the list *"
 echo "* 3. when done, type done *"
 echo "* 4. run make_command_list.sh *"
+echo "* 	run like \"./make_command_list.sh\" *";
+echo "* 5. Note: output may change based off version and distro used *"
