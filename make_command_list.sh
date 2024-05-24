8823c72a741ea480f3143513a0383c2b5247da9f
chad <chadb78999@gmail.com>
2024-05-23 21:56:53 -0500

    commented out test prints from script

--- make_command_list.sh
+++ make_command_list.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # print "Hello World!" to the bash shell in the terminal emulator 
-echo -e "Hello World!\n"
+#echo -e "Hello World!\n"
 #test array
-testArr=( 1 2 3 4 "here we go" );
-for i in "${testArr[@]}";
-do
-	echo $i;
-done;
-echo -e "\nTest Result:";
-man -k ls | grep ^ls\\s;
-echo -e "\nTest 2 looping through commands:";
+#testArr=( 1 2 3 4 "here we go" );
+#for i in "${testArr[@]}";
+#do
+#	echo $i;
+#done;
+#echo -e "\nTest Result:";
+#man -k ls | grep ^ls\\s;
+#echo -e "\nTest 2 looping through commands:";
 
 # commands=( "ls" "grep");
 # pulling commands from file
