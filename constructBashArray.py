e073b44ad69fabed42659c2bcdacd1b9b93e799f
chad <chadb78999@gmail.com>
2024-05-24 01:04:00 -0500

    made it so constructBashArray.py ignores duplicates and empty strings.

--- constructBashArray.py
+++ constructBashArray.py
@@ -2,9 +2,12 @@ print("**** HELPING YOU MAKE THE BASH SCRIPT ARRAY ****");
 word=""
 commandList=[];
 while(word!="done"):
-        word=input("Command to log: ");
-        if(word!="done"):
-            commandList.append(word);
+		word=input("Command to log: ");
+		if(word!="done" and len(word)!=0 and commandList.count(word)==0):
+			commandList.append(word);
+			#print("len({}):{}".format(word,len(word)));
+			#if(commandList.count(word)==0):
+				#commandList.append(word);
 
 print("commands=(",end="")
 for command in commandList:
